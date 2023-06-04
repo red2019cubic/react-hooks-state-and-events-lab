4742959b316b6fbe1149c66ac532f7f86a128782
Red <red.sarouf2014@gmail.com>
2023-06-04 05:01:32 -0400

    add state and events lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,21 @@
 import React from "react";
-
+import {useState}  from "react";
 function Item({ name, category }) {
+  const [inCart, setCart] = useState(false)
+
+  const addItemToCart = ()=>{
+    setCart(true);
+    
+  }
+
   return (
-    <li className="">
+    <li className={inCart ? 'in-cart': ''}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={addItemToCart} className="add">Add to Cart</button>
     </li>
   );
 }
+
 
 export default Item;
